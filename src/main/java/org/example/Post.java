3250ec9b24f10d2a2ac06e7964e931a1104aec79
Kseniya Radiseva <Kotaviy@gmail.com>
2023-06-25 14:19:48 +0500

    Initial commit

---
+++ src/main/java/org/example/Post.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Post {
+    public String patronymic;
+    public String name;
+    public String surname;
+    public FormDate birthday;
+    public String passport;
+    public String phone;
+    public boolean subscription;
+}
